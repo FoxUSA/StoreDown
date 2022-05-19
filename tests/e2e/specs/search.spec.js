a9030a44baa441eef58ddde559512d990b0d3dd9
Jake <jake@jakelis.com>
2022-05-19 09:36:46 -0700

    Using the cypress action to run tests

--- tests/e2e/specs/search.spec.js
+++ tests/e2e/specs/search.spec.js
@@ -1,21 +1,21 @@
-// describe('List View', () => {
-//   before(() => { //Don't need to reset anything
-//     cy.localMode()
-//     cy.loadTestData()
-//   })
+describe('List View', () => {
+  before(() => { //Don't need to reset anything
+    cy.localMode()
+    cy.loadTestData()
+  })
 
-//   it('can find a uuid', () => {
-//     cy.visit('/#/')// Go to the entry page
-//     cy.get('input[placeholder="Search items"').type('8e71919a-4f03-4fa4-bdce-5767e38ed48e{enter}')
-//     cy.contains('USB A to USB C cable')
+  it('can find a uuid', () => {
+    cy.visit('/#/')// Go to the entry page
+    cy.get('input[placeholder="Search items"').type('8e71919a-4f03-4fa4-bdce-5767e38ed48e{enter}')
+    cy.contains('USB A to USB C cable')
 
-//     //Partial uuid
-//     cy.get('input[placeholder="Search items"').invoke('val', '').type('1a76be8{enter}')
-//     cy.contains('Dental floss pick')
+    //Partial uuid
+    cy.get('input[placeholder="Search items"').invoke('val', '').type('1a76be8{enter}')
+    cy.contains('Dental floss pick')
 
-//     //Click search with name
-//     cy.get('input[placeholder="Search items"').invoke('val', '').type('USB A to USB C cable')
-//     cy.get('.v-btn__content').contains('Search').click()
-//     cy.contains('8ed48e')
-//   })
-// })
+    //Click search with name
+    cy.get('input[placeholder="Search items"').invoke('val', '').type('USB A to USB C cable')
+    cy.get('.v-btn__content').contains('Search').click()
+    cy.contains('8ed48e')
+  })
+})
